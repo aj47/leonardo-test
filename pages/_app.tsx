7dd37c3273dd6508caba81235d457613d216d94f
Arash Joobandi <arash@appricot.io>
2023-03-19 18:25:23 +1100

    nextauth, chakra, api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+const client = new ApolloClient({
+  uri: "https://countries.trevorblades.com/",
+  cache: new InMemoryCache(),
+});
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </ChakraProvider>
+    </ApolloProvider>
+  );
 }
